657815ceb3399464f96d759708d4cb473b532877
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-12-20 00:56:08 +0300

    added submit  event listener to search button

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,7 @@
 const apiKey = "64d07ebd73f88f10fc21d800abdfc343"
+
+const form = document.querySelector(".search-form");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const inputOfCity = input.value;
+});
