359c0078af4f1f8b2e5e92df86af5a828758e312
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-12-20 21:28:48 +0300

    made GET request to weather api

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,28 @@
 const apiKey = "64d07ebd73f88f10fc21d800abdfc343"
 
-const form = document.querySelector(".search-form");
+const input = document.getElementById('search-input')
+const inputVal = input.value;
+
+const form = document.querySelector("#search-form");
 form.addEventListener("submit", e => {
   e.preventDefault();
-  const inputOfCity = input.value;
+  let inputVal = input.value;
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&APPID=64d07ebd73f88f10fc21d800abdfc343&units=metric`)
+      .then(response => response.json())
+      .then(data => {
+        
+      })
+
+  //const inputVal = input.value;
 });
+
+//function getWeatherInfo(city){
+    //fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&APPID=64d07ebd73f88f10fc21d800abdfc343`)
+    //.then(response => response.json())
+    //.then(data => {console.log(data)})
+//}
+
+/*function initialize(){
+    getWeatherInfo()
+}
+initialize()*/
