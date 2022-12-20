82fbdc1050cb89e0e841229a89f7651b94cf6c00
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-12-20 23:23:07 +0300

    commented to explain some code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const apiKey = "64d07ebd73f88f10fc21d800abdfc343"
 const input = document.getElementById('search-input')
 const inputVal = input.value;
 
+//generate current time of each city
 d = new Date()
 localTime = d.getTime()
 localOffset = d.getTimezoneOffset() * 60000
@@ -18,6 +19,7 @@ form.addEventListener("submit", e => {
       .then(response => response.json())
       .then(data => {
         const{main, sys, name, weather} = data
+        //access relevant image depending on code for image in the api
         const weatherImage = `./icons/${weather[0]['icon']}.svg`
 
 
@@ -33,20 +35,9 @@ form.addEventListener("submit", e => {
                <div class="weather-description">${weather[0]['description']}</div>
                <div class="temp-range">${Math.round(main.temp_max)}°C/${Math.round(main.temp_min)}°C</div>
             </div>   `
-            
+        //append list to the ul element    
         document.querySelector(".city-weather").appendChild(list)    
       })
 
-  //const inputVal = input.value;
 });
 
-//function getWeatherInfo(city){
-    //fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&APPID=64d07ebd73f88f10fc21d800abdfc343`)
-    //.then(response => response.json())
-    //.then(data => {console.log(data)})
-//}
-
-/*function initialize(){
-    getWeatherInfo()
-}
-initialize()*/
